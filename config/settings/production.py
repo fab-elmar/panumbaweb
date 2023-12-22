c7f6f9b16f94c9a03cffe9ccd1bf98ade76149be
fab-elmar <fabsiban09@gmail.com>
2023-12-22 23:23:23 +0100

    allowed host

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"], "safe-dusk-88333-4a20cc240dc5.herokuapp.com",)
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"], )
 
 # DATABASES
 # ------------------------------------------------------------------------------
