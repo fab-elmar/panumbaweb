794746884cc8ca9da3e895421b73c2e3fc9c3d74
fab-elmar <fabsiban09@gmail.com>
2023-12-26 15:15:23 +0100

    change home url pointing to question

--- panumbaweb/interface/urls.py
+++ panumbaweb/interface/urls.py
@@ -5,7 +5,8 @@ from interface.api.views import LatestAnswerAPI
 
 app_name = "interface"
 urlpatterns = [
-    path("", AIcontactListView.as_view(), name="index"),
+    path("list/", AIcontactListView.as_view(), name="list"),
+     path("", PanumbaQuestionView.as_view(), name="index"),
     path("question/", PanumbaQuestionView.as_view(), name="question"),
     path('show-number/', ShowNumberView.as_view(), name='show_number'),
     path('api/latest-answer/', LatestAnswerAPI.as_view(), name='api_latest_answer'),
--- panumbaweb/static/js/project.js
+++ panumbaweb/static/js/project.js
@@ -1,13 +1,1 @@
 /* Project specific Javascript goes here. */
-
-
-function getCSRFToken() {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        if (cookie.substring(0, 10) == "csrftoken=") {
-            return decodeURIComponent(cookie.substring(10));
-        }
-    }
-    return "unknown";
-}
